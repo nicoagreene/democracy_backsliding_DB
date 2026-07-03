2ef982e2ed0581149cdb793eb0e8f64611fbe818
nicholasgreene <nicoagreene@gmail.com>
2026-07-03 12:24:05 -0400

    first commit

---
+++ spreadsheet_to_db.py
@@ -0,0 +1,16 @@
+"""
+DB schema:
+
+Fields per entry:
+id, article_name, publication_date, source_name, source_url, citation, violation_category, violation_description, summary, classification
+name_of_contributor, id_of_contributor, date_added,
+
+
+set_of_tag_values
+
+violation_categories: Immigration and deportations, Erasure and Censorship, Institutional Crackdowns, Individual rights and freedoms,
+Judicial Agency, Judicial Crackdown, Targeting/Civil Society Attacks, Civil society resistance, Violence, Foreign Policy
+
+classification: Social Contradiction, Norm Violation, Unlawful, Unconstitutional
+
+"""
